8e78e5fbb5af63d1898d378c6e22847941355ca6
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-01-31 17:14:20 -0300

    feature/GPT-2 Testing GTP Comments

---
+++ src/com/etendoerp/TestGPT.java
@@ -0,0 +1,23 @@
+package com.etendoerp;
+
+public class TestGPT {
+  public static int searchValue(int[] array, int value) {
+
+    if (array[0] == value) {
+      return value;
+    }
+    if (array[1] == value) {
+      return value;
+    }
+    if (array[2] == value) {
+      return value;
+    }
+    if (array[3] == value) {
+      return value;
+    }
+    if (array[4] == value) {
+      return value;
+    }
+    return -1;
+  }
+}
